e86ce597c7cd97649aee20cd765d38c9ca9d0530
Jonathan Swafford <jonathan.swafford@vumc.org>
2018-01-04 11:05:21 -0600

    Add syntax test

--- testomg/ViewController.swift
+++ testomg/ViewController.swift
@@ -2,24 +2,41 @@
 //  ViewController.swift
 //  testomg
 //
-//  Created by Swafford, Jonathan Anderson on 1/4/18.
+//  Created by Jonathan on 1/4/18.
 //  Copyright © 2018 Jonathan. All rights reserved.
 //
 
 import UIKit
+import PromiseKit
+import OMGHTTPURLRQ
+//import PMKOMGHTTPURLRQ <-- Error: No such module 'PMKOMGHTTPURLRQ'
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    static func testGetSyntax() -> Void {
+        
+        // OK
+        let p = URLSession.shared.GET("http://example.com")
+        p.then { (data) -> Void in
+            
+        }
+        
+        let q2 = ["foo": "bar",
+                  "baz": 1]
+        // Error: Cannot convert value of type 'String' to expected dictionary key type 'NSObject'
+        let p2 = URLSession.shared.GET("http://example.com", query: q2)
+        p2.then { (data) -> Void in
+        
+        }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Must cast all 'String' to 'NSString', 'Int' to 'NSNumber', etc...
+        let q3 = ["foo" as NSString: "bar" as NSString,
+                  "baz" as NSString: 1     as NSNumber]
+        let p3 = URLSession.shared.GET("http://example.com", query: q3)
+        p2.then { (data) -> Void in
+        
+        }
+        
     }
-
 
 }
-
